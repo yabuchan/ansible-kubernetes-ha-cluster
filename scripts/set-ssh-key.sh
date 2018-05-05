4519c8bc8f4dac5c62dbae05e8cec6d99c777e6b
Hitoshi Yabusaki <hitosh.yabusaki@gmail.com>
2018-05-04 22:32:20 -0700

    debugging

--- scripts/set-ssh-key.sh
+++ scripts/set-ssh-key.sh
@@ -3,7 +3,9 @@
 # 1. Setup ssh access from master to workers.
 # 2. Setup ssh access from first master to the other masters (optional to be able to use scp)
 # You may manually do above two tasks instead of running this script.
-
-ssh-keygen -t rsa -b 2048 -f .ssh/id_rsa # Generate ssh key
+mkdir -p ../.ssh
+ssh-keygen -t rsa -b 2048 -f ../.ssh/id_rsa # Generate ssh key
 cd ../
+cp ../.ssh/id_rsa ~/.ssh/id_rsa
+cp ../.ssh/id_rsa.pub ~/.ssh/id_rsa.pub
 ansible-playbook -i inventory/mycluster playbooks/copy-ssh-key-id.yaml
