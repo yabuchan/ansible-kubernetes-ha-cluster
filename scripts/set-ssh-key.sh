d04c3cb4f82b594e4cf3c64229c1772eb0025ac2
Hitoshi Yabusaki <hitosh.yabusaki@gmail.com>
2018-05-05 19:10:15 -0700

    set-ssh-key is fixed.

--- scripts/set-ssh-key.sh
+++ scripts/set-ssh-key.sh
@@ -5,8 +5,18 @@
 # You may manually do above two tasks instead of running this script.
 mkdir -p ../_ssh && ssh-keygen -t rsa -b 2048 -N "" -f ../_ssh/id_rsa # Generate ssh key
 
-cd ../
-ansible-playbook -i inventory/mycluster playbooks/copy-ssh-key-id.yaml --private-key=$PRIVATE_KEY
+cp ../inventory/mycluster ../inventory/mycluster-init
+sed -i -e 's/centos/root/g' ../inventory/mycluster-init
 
-# access using password is discouraged, but if you want, you can do it with:
-# ansible-playbook -i inventory/mycluster playbooks/copy-ssh-key-id.yaml --ask-pass
+cd ../ && \
+  ansible-playbook -i inventory/mycluster playbooks/copy-ssh-key-id.yaml --private-key=$PRIVATE_KEY && \
+
+  # access using password is discouraged, but if you want, you can do it with:
+  # ansible-playbook -i inventory/mycluster playbooks/copy-ssh-key-id.yaml --ask-pass
+
+  # Check connectivity
+  ansible -i inventory/mycluster-init -m ping all-vms
+cd scripts
+
+# remove temporal init file
+rm -f ../inventory/mycluster-init
